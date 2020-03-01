167d9b8d9019f7b43f987fc2d08e247d08934bda
b0kch01 <nath49ers@gmail.com>
2020-02-29 20:44:05 -0800

    fix js init

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,11 @@
+if ($(window).width() < 897) {
+	$(".shrink").hide();
+	console.log("shrink");
+} else {
+	$(".shrink").show();
+	console.log("unshrink");
+}
+
 $(window).on("resize", function() {
 	if ($(window).width() < 897) {
 		$(".shrink").hide();
