fa25d2a939b9dd095f4265c76eea5f677f2370e8
b0kch01 <nath49ers@gmail.com>
2020-02-29 20:42:38 -0800

    FINALLY IT IS FULLY RESPONSIVE

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+$(window).on("resize", function() {
+	if ($(window).width() < 897) {
+		$(".shrink").hide();
+		console.log("shrink");
+	} else {
+		$(".shrink").show();
+		console.log("unshrink");
+	}
+});
