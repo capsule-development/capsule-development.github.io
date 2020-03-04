3520d99792a75989f0db640e719f680b854b9c96
Corban Amouzou <c0dineDev@gmail.com>
2020-03-04 12:05:09 -0800

    create firebaseConfig.js
    create firebaseConfig.js 
    and remove the config from the main page (as it could lead to vulnerability's)

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+		var firebaseConfig = {
+			apiKey: "AIzaSyBi-8qI5QDYPLAExPbLectwpD-88arhN3Y",
+			authDomain: "capsule-manager.firebaseapp.com",
+			databaseURL: "https://capsule-manager.firebaseio.com",
+			projectId: "capsule-manager",
+			storageBucket: "capsule-manager.appspot.com",
+			messagingSenderId: "10045598935",
+			appId: "1:10045598935:web:106c61b326a13af7cad492",
+			measurementId: "G-F2XDB5FRFM"
+		};
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig);
+		firebase.analytics();
+	
