a0e7a3ee328a7f33f30eddfda6566a4fa782bbf2
b0kch01 <nath49ers@gmail.com>
2020-03-14 21:13:04 -0700

    sign in ui done!

---
+++ assets/js/auth.js
@@ -0,0 +1,6 @@
+// Signup JS
+const signupForm = document.querySelector("#signup-form");
+
+// signup-form.addEventListener("submit", (e) => {
+// 	e.preventDefault();
+// })
--- assets/js/inputs.js
+++ assets/js/inputs.js
@@ -2,11 +2,12 @@ console.log("// FORMS INPUT CSS BY MARY LOU //\n" +
             "Here is her site: http://www.codrops.com\n" +
             "Great thanks to all open-sourcers!");
 
+// This is to change to label text
 $("#email > input").on("keyup paste", function() {
     if ($("#email > input").val() == "") {
         $("#email > label > span").text("Enter your email.");
     } else {
-        $("#email > label > span").text("Email: " + $("#email > input").val());
+        $("#email > label > span").text("Email:");
     }
 });
 
@@ -14,6 +15,42 @@ $("#pswd > input").on("keyup paste", function() {
     if ($("#pswd > input").val() == "") {
         $("#pswd > label > span").text("Enter your password.");
     } else {
-        $("#pswd > label > span").text("Pass: " + $("#pswd > input").val());
+        $("#pswd > label > span").text("Password:");
     }
 });
+
+
+// Makes sure that the input is always focused if there is text
+(function() {
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+        (function() {
+            // Make sure we trim BOM and NBSP
+            var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            String.prototype.trim = function() {
+                return this.replace(rtrim, '');
+            };
+        })();
+    }
+
+    [].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+        // in case the input is already filled..
+        if( inputEl.value.trim() !== '' ) {
+            classie.add( inputEl.parentNode, 'input--filled' );
+        }
+
+        // events:
+        inputEl.addEventListener( 'focus', onInputFocus );
+        inputEl.addEventListener( 'blur', onInputBlur );
+    } );
+
+    function onInputFocus( ev ) {
+        classie.add( ev.target.parentNode, 'input--filled' );
+    }
+
+    function onInputBlur( ev ) {
+        if( ev.target.value.trim() === '' ) {
+            classie.remove( ev.target.parentNode, 'input--filled' );
+        }
+    }
+})();
