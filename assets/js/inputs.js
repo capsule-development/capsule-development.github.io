3628adce2cf1a900615dc3b226dee2fd2e3f2357
b0kch01 <nath49ers@gmail.com>
2020-03-14 20:22:59 -0700

    js in inputs!

--- assets/js/inputs.js
+++ assets/js/inputs.js
@@ -1,17 +1,19 @@
-console.log("Something happened")
+console.log("// FORMS INPUT CSS BY MARY LOU //\n" + 
+            "Here is her site: http://www.codrops.com\n" +
+            "Great thanks to all open-sourcers!");
 
 $("#email > input").on("keyup paste", function() {
     if ($("#email > input").val() == "") {
-        $("#email > span").text("Enter your email.");
+        $("#email > label > span").text("Enter your email.");
     } else {
-        $("#email > span").text($("#email > input").val());
+        $("#email > label > span").text("Email: " + $("#email > input").val());
     }
 });
 
 $("#pswd > input").on("keyup paste", function() {
     if ($("#pswd > input").val() == "") {
-        $("#pswd > span").text("Enter your password.");
+        $("#pswd > label > span").text("Enter your password.");
     } else {
-        $("#pswd > span").text($("#pswd > input").val());
+        $("#pswd > label > span").text("Pass: " + $("#pswd > input").val());
     }
 });
